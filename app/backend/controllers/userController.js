bed3a9a651a2590876c4c6ca1248459c90d23822
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-26 15:09:14 +0530

    Update userController.js

--- app/backend/controllers/userController.js
+++ app/backend/controllers/userController.js
@@ -1,1 +1,11 @@
 
+const User = require('../models/User');
+
+// GET all users 
+const getAllUsers = async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+};
+
+module.exports = { getAllUsers };
+
