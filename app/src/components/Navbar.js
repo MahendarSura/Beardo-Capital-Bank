634be9f41b57cc02e16396768a59aea352160fb5
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-26 14:55:37 +0530

    Update Navbar.js

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,1 +1,13 @@
 
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <h1 className="text-xl font-bold">Beardo Capital Bank</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
+
