e27ec937e6267395d3c6236041aae632325971c2
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-26 15:10:05 +0530

    Update courseRoutes.js

--- app/backend/routes/courseRoutes.js
+++ app/backend/routes/courseRoutes.js
@@ -1,1 +1,10 @@
 
+const express = require('express');
+const { getAllCourses } = require('../controllers/courseController');
+
+const router = express.Router();
+
+router.get('/', getAllCourses);
+
+module.exports = router;
+
