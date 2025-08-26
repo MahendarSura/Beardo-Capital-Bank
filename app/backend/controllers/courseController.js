2e7233bf344e2e5f4019f73174fc880b979c89bc
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-26 15:08:58 +0530

    Update courseController.js

--- app/backend/controllers/courseController.js
+++ app/backend/controllers/courseController.js
@@ -1,1 +1,10 @@
 
+const Course = require('../models/Course');
+
+const getAllCourses = async (req, res) => {
+  const courses = await Course.find({}).populate('instructor', 'name');
+  res.json(courses);
+};
+
+module.exports = { getAllCourses };
+
