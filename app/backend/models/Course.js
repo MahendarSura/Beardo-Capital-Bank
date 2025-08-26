cae600edf2adc7201caf204a740ac740f10eb8a5
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-26 15:09:32 +0530

    Update Course.js

--- app/backend/models/Course.js
+++ app/backend/models/Course.js
@@ -1,1 +1,12 @@
 
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  content: String,
+  instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Course', courseSchema);
+
